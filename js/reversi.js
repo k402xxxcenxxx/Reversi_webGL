aa3aa7648ea6833d5d1323c5b64878506de9df89
k402xxxcenxxx <k402xxxcenxxx@gmail.com>
2016-03-03 23:09:44 +0800

    大部分改完

--- js/reversi.js
+++ js/reversi.js
@@ -43,9 +43,9 @@ function Checkerboard(){
         chessManualIndex = -1;
         step = -1;
         currentChess = chess.whiteChess;
-        for (int j = 0;j < sideLength;j++)
+        for (var j = 0;j < sideLength;j++)
         {
-            for (int i = 0;i < sideLength;i++)
+            for (var i = 0;i < sideLength;i++)
             {
                 if ((i == 3 && j == 3) || (i == 4 && j == 4))
                 {
@@ -162,32 +162,517 @@ function Checkerboard(){
         }
     }
 
-    function cursorMove(int input);//0 = left,1 = up,2 = down,3 = right
+    function cursorMove(input){
+        switch(input){
+            case 0://left
+                if(currentPosition[0]-1 >= 0)
+                    currentPosition[0] -= 1;
+                break;
+            case 1://up
+                if(currentPosition[1]-1 >= 0)
+                    currentPosition[1] -= 1;
+                break;
+            case 2://down
+                if(currentPosition[1]+1 < 8)
+                    currentPosition[1] += 1;
+                break;
+            case 3://right
+                if(currentPosition[0]+1 < 8)
+                    currentPosition[0] += 1;
+                break;
 
-    function saveCurrentChessManual(void);
+        }
+    }//0 = left,1 = up,2 = down,3 = right
 
-    function undo(void);
-    function redo(void);
-    function checkPass(void);
+    function saveCurrentChessManual(){
+        blackCount = 0;
+        whiteCount = 0;
 
-    function checkRight(PosX, PosY);//return gain chesss. if it returns less than 0, means it's not legal
-    function checkLeft(PosX, PosY);//return gain chesss. if it returns less than 0, means it's not legal
-    function checkUp(PosX, PosY);//return gain chesss. if it returns less than 0, means it's not legal
-    function checkDown(PosX, PosY);//return gain chesss. if it returns less than 0, means it's not legal
-    function checkRightUp(PosX, PosY);//return gain chesss. if it returns less than 0, means it's not legal
-    function checkRightDown(PosX, PosY);//return gain chesss. if it returns less than 0, means it's not legal
-    function checkLeftUp(PosX,PosY);//return gain chesss. if it returns less than 0, means it's not legal
-    function checkLeftDown(PosX,PosY);//return gain chesss. if it returns less than 0, means it's not legal
+        step++;
+        chessManualIndex++;
+        //rewrite step count
+        if (chessManualIndex <= step){
+            step = chessManualIndex;
+        }
 
-    function checkLegal(int PosX,int PosY);//check if legal
+        for (var rowNum = 0; rowNum < sideLength; rowNum++)
+        {
+            for (var colNum = 0; colNum < sideLength; colNum++)
+            {
+                chessManual[chessManualIndex][rowNum][colNum] = checkerboardInfo[rowNum][colNum];
 
-    function checkFinished(void);
+                switch (checkerboardInfo[rowNum][colNum]){
+                    case InfoValue.black:
+                        blackCount++;
+                        break;
+                    case InfoValue.white:
+                        whiteCount++;
+                        break;
+                }
+            }
+        }
+
+        if (currentChess == chess.whiteChess){
+            currentChess = chess.blackChess;
+        }
+        else if (currentChess == chess.blackChess){
+            currentChess = chess.whiteChess;
+        }
+
+        //if no pass,exchange chess
+        if (checkPass()){
+            if (currentChess == chess.whiteChess){
+                currentChess = chess.blackChess;
+            }
+            else if (currentChess == chess.blackChess){
+                currentChess = chess.whiteChess;
+            }
+
+            if (checkPass()){
+                isFinished = true;
+            }
+        }
+
+        chessRecord[chessManualIndex] = currentChess;
+
+    }
+
+    function undo(){
+        if (chessManualIndex - 1 >= 0){
+            chessManualIndex -= 1;
+        }
+
+        //overwrite info
+        for (int rowNum = 0; rowNum < sideLength; rowNum++)
+        {
+            for (int colNum = 0; colNum < sideLength; colNum++)
+            {
+                checkerboardInfo[rowNum][colNum] = chessManual[chessManualIndex][rowNum][colNum];
+            }
+        }
+
+        currentChess = chessRecord[chessManualIndex];
+
+    }
+
+    function redo(){
+        if (chessManualIndex + 1 <= step){
+            chessManualIndex += 1;
+        }
+
+
+        //overwrite info
+        for (int rowNum = 0; rowNum < sideLength; rowNum++)
+        {
+            for (int colNum = 0; colNum < sideLength; colNum++)
+            {
+                checkerboardInfo[rowNum][colNum] = chessManual[chessManualIndex][rowNum][colNum];
+            }
+        }
+        currentChess = chessRecord[chessManualIndex];
+    }
+
+    function checkPass(){
+        //check no hint
+        for (var rowNum = 0; rowNum < sideLength; rowNum++)
+        {
+            for (var colNum = 0; colNum < sideLength; colNum++)
+            {
+                if (checkerboardInfo[rowNum][colNum] == InfoValue.none){
+                    if (checkLegal(colNum, rowNum)){
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+
+    function checkRight(PosX,PosY){
+        var myValue, enemyValue;
+        var rowNum = PosY;
+        var colNum = PosX + 1;
+        var gainValue = 0;
+
+        //comfirm target chess
+        if (currentChess == chess.blackChess){
+            myValue = InfoValue.black;
+            enemyValue = InfoValue.white;
+        }
+        else if (currentChess == chess.whiteChess){
+            myValue = InfoValue.white;
+            enemyValue = InfoValue.black;
+        }
+
+        //beside must be enemy
+        if (checkerboardInfo[rowNum][colNum] == enemyValue){
+            gainValue++;
+
+            //check from left to right
+            for (colNum+=1; colNum < sideLength; colNum++){
+                if (checkerboardInfo[rowNum][colNum] == myValue){
+                    //finish gain chess
+                    return gainValue;
+                }
+                else if (checkerboardInfo[rowNum][colNum] == enemyValue){
+                    gainValue++;
+                }
+                else if (checkerboardInfo[rowNum][colNum] == InfoValue.none){
+                    return -1;
+                }
+            }
+
+            //there is no my chess on the other side
+            return -1;
+        }
+        else{
+            return -1;
+        }
+
+    }
+
+    function checkLeft(PosX,PosY){
+        var myValue, enemyValue;
+        var rowNum = PosY;
+        var colNum = PosX - 1;
+        var gainValue = 0;
+
+        //comfirm target chess
+        if (currentChess == chess.blackChess){
+            myValue = InfoValue.black;
+            enemyValue = InfoValue.white;
+        }
+        else if (currentChess == chess.whiteChess){
+            myValue = InfoValue.white;
+            enemyValue = InfoValue.black;
+        }
+
+        //beside must be enemy
+        if (checkerboardInfo[rowNum][colNum] == enemyValue){
+            gainValue++;
+
+            //check from right to left  
+            for (colNum -= 1; colNum >= 0; colNum--){
+                if (checkerboardInfo[rowNum][colNum] == myValue){
+                    //finish gain chess
+                    return gainValue;
+                }
+                else if (checkerboardInfo[rowNum][colNum] == enemyValue){
+                    gainValue++;
+                }
+                else if (checkerboardInfo[rowNum][colNum] == InfoValue.none){
+                    return -1;
+                }
+            }
+
+            //there is no my chess on the other side
+            return -1;
+        }
+        else{
+            return -1;
+        }
+
+    }
+
+    function checkUp(PosX,PosY){
+        var myValue, enemyValue;
+        var rowNum = PosY - 1;
+        var colNum = PosX;
+        var gainValue = 0;
+
+        //comfirm target chess
+        if (currentChess == chess.blackChess){
+            myValue = InfoValue.black;
+            enemyValue = InfoValue.white;
+        }
+        else if (currentChess == chess.whiteChess){
+            myValue = InfoValue.white;
+            enemyValue = InfoValue.black;
+        }
+
+        //beside must be enemy
+        if (checkerboardInfo[rowNum][colNum] == enemyValue){
+            gainValue++;
+
+            //check from down to up  
+            for (rowNum -= 1; rowNum >= 0; rowNum--){
+                if (checkerboardInfo[rowNum][colNum] == myValue){
+                    //finish gain chess
+                    return gainValue;
+                }
+                else if (checkerboardInfo[rowNum][colNum] == enemyValue){
+                    gainValue++;
+                }
+                else if (checkerboardInfo[rowNum][colNum] == InfoValue.none){
+                    return -1;
+                }
+            }
+
+            //there is no my chess on the other side
+            return -1;
+        }
+        else{
+            return -1;
+        }
+
+    }
+
+    function checkDown(PosX,PosY){
+        var myValue, enemyValue;
+        var rowNum = PosY + 1;
+        var colNum = PosX;
+        var gainValue = 0;
+
+        //comfirm target chess
+        if (currentChess == chess.blackChess){
+            myValue = InfoValue.black;
+            enemyValue = InfoValue.white;
+        }
+        else if (currentChess == chess.whiteChess){
+            myValue = InfoValue.white;
+            enemyValue = InfoValue.black;
+        }
+
+        //beside must be enemy
+        if (checkerboardInfo[rowNum][colNum] == enemyValue){
+            gainValue++;
+
+            //check from up to down  
+            for (rowNum += 1; rowNum < sideLength; rowNum++){
+                if (checkerboardInfo[rowNum][colNum] == myValue){
+                    //finish gain chess
+                    return gainValue;
+                }
+                else if (checkerboardInfo[rowNum][colNum] == enemyValue){
+                    gainValue++;
+                }
+                else if (checkerboardInfo[rowNum][colNum] == InfoValue.none){
+                    return -1;
+                }
+            }
+
+            //there is no my chess on the other side
+            return -1;
+        }
+        else{
+            return -1;
+        }
+
+    }
+
+    function checkRightUp(PosX,PosY){
+        var myValue, enemyValue;
+        var rowNum = PosY - 1;
+        var colNum = PosX + 1;
+        var gainValue = 0;
+
+        //comfirm target chess
+        if (currentChess == chess.blackChess){
+            myValue = InfoValue.black;
+            enemyValue = InfoValue.white;
+        }
+        else if (currentChess == chess.whiteChess){
+            myValue = InfoValue.white;
+            enemyValue = InfoValue.black;
+        }
+
+        //beside must be enemy
+        if (checkerboardInfo[rowNum][colNum] == enemyValue){
+            gainValue++;
+
+            //check from leftdown to rightup  
+            for (rowNum -= 1, colNum += 1; rowNum >= 0 && colNum < sideLength; rowNum--, colNum++){
+                if (checkerboardInfo[rowNum][colNum] == myValue){
+                    //finish gain chess
+                    return gainValue;
+                }
+                else if (checkerboardInfo[rowNum][colNum] == enemyValue){
+                    gainValue++;
+                }
+                else if (checkerboardInfo[rowNum][colNum] == InfoValue.none){
+                    return -1;
+                }
+            }
+
+            //there is no my chess on the other side
+            return -1;
+        }
+        else{
+            return -1;
+        }
+
+    }
+
+    function checkRightDown(PosX,PosY){
+        var myValue, enemyValue;
+        var rowNum = PosY + 1;
+        var colNum = PosX + 1;
+        var gainValue = 0;
+
+        //comfirm target chess
+        if (currentChess == chess.blackChess){
+            myValue = InfoValue.black;
+            enemyValue = InfoValue.white;
+        }
+        else if (currentChess == chess.whiteChess){
+            myValue = InfoValue.white;
+            enemyValue = InfoValue.black;
+        }
+
+        //beside must be enemy
+        if (checkerboardInfo[rowNum][colNum] == enemyValue){
+            gainValue++;
+
+            //check from leftup to rightdown  
+            for (rowNum += 1, colNum += 1; rowNum < sideLength && colNum < sideLength; rowNum++, colNum++){
+                if (checkerboardInfo[rowNum][colNum] == myValue){
+                    //finish gain chess
+                    return gainValue;
+                }
+                else if (checkerboardInfo[rowNum][colNum] == enemyValue){
+                    gainValue++;
+                }
+                else if (checkerboardInfo[rowNum][colNum] == InfoValue.none){
+                    return -1;
+                }
+            }
+
+            //there is no my chess on the other side
+            return -1;
+        }
+        else{
+            return -1;
+        }
+
+    }
+
+    function checkLeftUp(PosX,PosY){
+        var myValue, enemyValue;
+        var rowNum = PosY - 1;
+        var colNum = PosX - 1;
+        var gainValue = 0;
+
+        //comfirm target chess
+        if (currentChess == chess.blackChess){
+            myValue = InfoValue.black;
+            enemyValue = InfoValue.white;
+        }
+        else if (currentChess == chess.whiteChess){
+            myValue = InfoValue.white;
+            enemyValue = InfoValue.black;
+        }
+
+        //beside must be enemy
+        if (checkerboardInfo[rowNum][colNum] == enemyValue){
+            gainValue++;
+
+            //check from rightdown to leftup  
+            for (rowNum -= 1, colNum -= 1; rowNum >= 0 && colNum >= 0; rowNum--, colNum--){
+                if (checkerboardInfo[rowNum][colNum] == myValue){
+                    //finish gain chess
+                    return gainValue;
+                }
+                else if (checkerboardInfo[rowNum][colNum] == enemyValue){
+                    gainValue++;
+                }
+                else if (checkerboardInfo[rowNum][colNum] == InfoValue.none){
+                    return -1;
+                }
+            }
+
+            //there is no my chess on the other side
+            return -1;
+        }
+        else{
+            return -1;
+        }
+
+    }
+
+    function checkLeftDown(PosX,PosY){
+        var myValue, enemyValue;
+        var rowNum = PosY + 1;
+        var colNum = PosX - 1;
+        var gainValue = 0;
+
+        //comfirm target chess
+        if (currentChess == chess.blackChess){
+            myValue = InfoValue.black;
+            enemyValue = InfoValue.white;
+        }
+        else if (currentChess == chess.whiteChess){
+            myValue = InfoValue.white;
+            enemyValue = InfoValue.black;
+        }
+
+        //beside must be enemy
+        if (checkerboardInfo[rowNum][colNum] == enemyValue){
+            gainValue++;
+
+            //check from rightup to leftdown  
+            for (rowNum += 1, colNum -= 1; rowNum < sideLength && colNum >= 0; rowNum++, colNum--){
+                if (checkerboardInfo[rowNum][colNum] == myValue){
+                    //finish gain chess
+                    return gainValue;
+                }
+                else if (checkerboardInfo[rowNum][colNum] == enemyValue){
+                    gainValue++;
+                }
+                else if (checkerboardInfo[rowNum][colNum] == InfoValue.none){
+                    return -1;
+                }
+            }
+
+            //there is no my chess on the other side
+            return -1;
+        }
+        else{
+            return -1;
+        }
+
+    }
+    function checkLegal(PosX,PosY){
+        if (checkerboardInfo[PosY][PosX] != InfoValue.none){
+            return false;
+        }
+
+        if (checkRight(PosX, PosY) > 0 ||
+            checkLeft(PosX, PosY) > 0 ||
+            checkUp(PosX, PosY) > 0 ||
+            checkDown(PosX, PosY) > 0 ||
+            checkRightUp(PosX, PosY) > 0 ||
+            checkRightDown(PosX, PosY) > 0 ||
+            checkLeftUp(PosX, PosY) > 0 ||
+            checkLeftDown(PosX, PosY) > 0){
+
+            return true;
+        }
+        else{
+            return false;
+        }
+    }//check if legal
+
+    function checkFinished(){
+        if (step >= 60){
+            isFinished = true;
+        }else{
+            isFinished = false;
+        }
+    }
 
     var Log;
     var showedLog;
-    function logHandler(log);
-    function showLog();
-    function clearLog(void);
+    function logHandler(log){
+        Log += log;
+    }
+    
+    function showLog(){}
+    
+    function clearLog(){
+        Log = "";
+    }
 
-    function toString(void);
+    function getInfo(){
+        return checkerboardInfo;
+    }
 };
